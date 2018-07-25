ce370d909545aacd01f79150dce303d1ed2e89ae
Soggard <e.bernet@laposte.net>
2018-07-25 12:30:11 +0200

    Clicker fonctionnel, gestion de l'autoclick

--- kenny-killer/src/components/Counter.vue
+++ kenny-killer/src/components/Counter.vue
@@ -1,31 +1,50 @@
 <template>
     <div>
-        <h1 class="count">{{count}}</h1> <br>
+        <h1 class="count">{{ totalCookies }}</h1> <br>
         <img src="../assets/Kenny.png" alt="kenny" @click="increment">
     </div>
 </template>
 
 
 <script>
-  import { mapActions, mapGetters } from "vuex"
+  import { mapActions, mapGetters, mapState } from "vuex";
+  //import App from './App'
+  //import { store } from '../store.js';
 
   export default {
-    name: 'Counter',
+      name: 'Counter',
       data() {
         return {
-          count: 0
+          interval:''
         }
       },
       computed: {
-        ...mapGetters["count"]
+          //...mapGetters["count"],
+          totalCookies: function () {
+              return this.$store.state.countTotal;
+          }
       },
       methods: {
-        ...mapActions["incrementCount"],
+        //...mapActions["incrementCount"],
         increment() {
-          this.count++;
-          console.log(mapGetters["count"]);
+          //this.count++;
+          this.$store.commit('ClickKenny');
+          //console.log(this.$store.);
+        },
+        autoClick: function(){
+            this.interval = setInterval(() => {
+                this.$store.commit('AutoKill');
+                console.log('tick');
+                console.log(this.$store.state.killPerSecond);
+            }, 1000);
         }
-      }
+      },
+      mounted : function(){
+          this.autoClick();
+      },
+      beforeDestroy () {
+          clearInterval(this.interval)
+      },
       // Pour envoyer un "commit" au store : this.$store.commit('NOMDUCOMMIT', value)
   }
 </script>
